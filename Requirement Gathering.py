355c95ccf457bf3524ba20c17a9415c924617b1f
balajiabadar12 <balaji.abadar.praj@gmail.com>
2023-01-01 11:25:11 +0530

    This is Requirement Gathering commit

---
+++ Requirement Gathering.py
@@ -0,0 +1,10 @@
+# This is file for requirement Gathering from client:
+
+# Write a code for printing 1 to 50 even numbers:
+
+Range1 = range (1,50)
+for i in Range1:
+    if i%2 ==0:
+        print(i)
+    i+=1
+
