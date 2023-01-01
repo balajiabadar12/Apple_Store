47578337845f3b5c6ce41c0d6f2a84643f44ca3b
balajiabadar12 <balaji.abadar.praj@gmail.com>
2023-01-01 10:44:57 +0530

    First commit-main

---
+++ main.py
@@ -0,0 +1,16 @@
+# ***Coding Starts Here***
+
+# Getting user inputs:
+First_Number= int(input("Enter the first Number: "))
+Second_Number= int(input("Enter the Second Number: "))
+
+# Comparision Programme for two numbers:
+if First_Number >= Second_Number:
+    if First_Number == Second_Number:
+        print("First and second number are same")
+    else:
+        print("First Number is greater than second number")
+
+else:
+    print("Second number is greater than first Number")
+
